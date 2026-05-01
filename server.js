804a49a682527080d79c6db763c6c9b069865246
SharifuzzamanShaon <shasan2200@gmail.com>
2026-05-01 12:54:50 +0600

    HOST_URL added to cors

--- server.js
+++ server.js
@@ -11,14 +11,11 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: [
-      "http://localhost:3000",
-      "http://localhost:3001",
-    ],
+    origin: ["http://localhost:3000", "http://localhost:3001", HOST_URL],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "Refresh-Token"],
-  })
+  }),
 );
 // Serve static files
 // file
